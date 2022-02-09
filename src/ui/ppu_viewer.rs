bd9605b4a39883fb49e1983a98b8f94882616367
Rodrigo Moraes <rodrigobatsmoraes@hotmail.com>
2022-02-09 16:43:48 -0300

    enhance ppu viewer

--- src/ui/ppu_viewer.rs
+++ src/ui/ppu_viewer.rs
@@ -4,7 +4,7 @@ use crui::graphics::{Graphic, Texture};
 use crui::layouts::{FitText, GridLayout, HBoxLayout, VBoxLayout};
 use crui::text::Text;
 use crui::widgets::{ScrollBar, ScrollView, ViewLayout};
-use crui::{Behaviour, BuilderContext, Context, Id, InputFlags, MouseEvent, Color};
+use crui::{Behaviour, BuilderContext, Color, Context, Id, InputFlags, MouseEvent};
 use gameroy::gameboy::GameBoy;
 use parking_lot::Mutex;
 use winit::event_loop::EventLoopProxy;
@@ -23,6 +23,11 @@ struct TilemapViewer {
     info: Box<dyn FnMut(u8, u8, &mut Context) -> String>,
 }
 impl Behaviour for TilemapViewer {
+    fn on_start(&mut self, _this: Id, ctx: &mut Context) {
+        let info = (self.info)(0, 0, ctx);
+        ctx.get_graphic_mut(self.info_text).set_text(&info);
+    }
+
     fn input_flags(&self) -> InputFlags {
         InputFlags::MOUSE
     }
@@ -56,117 +61,115 @@ struct PpuViewer {
     _frame_updated_event: Handle<FrameUpdated>,
     _emulator_updated_event: Handle<EmulatorUpdated>,
 }
+impl PpuViewer {
+    fn update(&mut self, ctx: &mut Context, emulator_updated: bool) {
+        let textures = ctx.get::<Textures>();
+        const COLOR: [[u8; 3]; 4] = [[255, 255, 255], [170, 170, 170], [85, 85, 85], [0, 0, 0]];
+        let gb = ctx.get::<Arc<Mutex<GameBoy>>>();
+        let proxy = &ctx.get::<EventLoopProxy<UserEvent>>();
+        let ppu = gb.lock().ppu.clone();
+        if emulator_updated {
+            let mut debug_screen = vec![255; 160 * 144 * 4];
+            let screen = &ppu.screen;
+            let curr_i = (ppu.ly as usize * 160 + ppu.curr_x as usize).min(160 * 144);
+
+            for i in 0..curr_i {
+                let c = screen[i];
+                let i = i * 4;
+                debug_screen[i..i + 3].copy_from_slice(&COLOR[c as usize]);
+            }
+
+            const OLD_COLOR: [[u8; 3]; 4] = [[0, 0, 200], [0, 0, 110], [0, 0, 85], [0, 0, 60]];
+            for i in curr_i..screen.len() {
+                let c = screen[i];
+                let i = i * 4;
+                debug_screen[i..i + 3].copy_from_slice(&OLD_COLOR[c as usize]);
+            }
+
+            proxy
+                .send_event(UserEvent::UpdateTexture(
+                    textures.screen,
+                    debug_screen.into_boxed_slice(),
+                ))
+                .unwrap();
+        }
+        let mut tiles = vec![255; 128 * 194 * 4];
+        gameroy::ppu::draw_tiles(
+            &ppu,
+            &mut |x, y, c| {
+                let i = (x + y * 128) as usize * 4;
+                let color = COLOR[c as usize];
+                tiles[i..i + 3].copy_from_slice(&color);
+            },
+            ppu.bgp,
+        );
+        proxy
+            .send_event(UserEvent::UpdateTexture(
+                textures.tilemap,
+                tiles.into_boxed_slice(),
+            ))
+            .unwrap();
+        let mut background = vec![255; 256 * 256 * 4];
+        gameroy::ppu::draw_background(&ppu, &mut |x, y, c| {
+            let i = (x + y * 256) as usize * 4;
+            background[i..i + 3].copy_from_slice(&COLOR[c as usize]);
+        });
+        proxy
+            .send_event(UserEvent::UpdateTexture(
+                textures.background,
+                background.into_boxed_slice(),
+            ))
+            .unwrap();
+        let mut window = vec![255; 256 * 256 * 4];
+        gameroy::ppu::draw_window(&ppu, &mut |x, y, c| {
+            let i = (x + y * 256) as usize * 4;
+            window[i..i + 3].copy_from_slice(&COLOR[c as usize]);
+        });
+        proxy
+            .send_event(UserEvent::UpdateTexture(
+                textures.window,
+                window.into_boxed_slice(),
+            ))
+            .unwrap();
+        for (i, &[view, text]) in self.oam_tiles.iter().enumerate() {
+            let i = i * 4;
+            let data = &ppu.oam[i..i + 4];
+            let sy = data[0];
+            let sx = data[1];
+            let tile = data[2];
+            let flags = data[3];
+
+            // let palette = if flags & 0x10 != 0 {
+            //     ppu.obp1
+            // } else {
+            //     ppu.obp0
+            // };
+
+            let x = tile % 16;
+            let y = tile / 16;
+            let uv_rect = [x as f32 / 16.0, y as f32 / 24.0, 1.0 / 16.0, 1.0 / 24.0];
+
+            if let Graphic::Texture(t) = ctx.get_graphic_mut(view) {
+                if sy < 16 || sx < 8 {
+                    t.color = 0xff0000ff.into();
+                } else {
+                    t.color = Color::WHITE;
+                }
+                t.uv_rect = uv_rect;
+            }
+
+            ctx.get_graphic_mut(text).set_text(&format!(
+                "x: {:02x} y: {:02x}\ntile: {:02x}\nflag: {:02x}",
+                sx, sy, tile, flags
+            ))
+        }
+    }
+}
 impl Behaviour for PpuViewer {
     fn on_event(&mut self, event: Box<dyn std::any::Any>, _this: Id, ctx: &mut crui::Context) {
         if event.is::<FrameUpdated>() || event.is::<EmulatorUpdated>() {
-            let textures = ctx.get::<Textures>();
-            const COLOR: [[u8; 3]; 4] = [[255, 255, 255], [170, 170, 170], [85, 85, 85], [0, 0, 0]];
-
-            let gb = ctx.get::<Arc<Mutex<GameBoy>>>();
-            let proxy = &ctx.get::<EventLoopProxy<UserEvent>>();
-            let ppu = gb.lock().ppu.clone();
-
-            if event.is::<EmulatorUpdated>() {
-                let mut debug_screen = vec![255; 160 * 144 * 4];
-                let screen = &ppu.screen;
-                let curr_i = (ppu.ly as usize * 160 + ppu.curr_x as usize).min(160 * 144);
-
-                for i in 0..curr_i {
-                    let c = screen[i];
-                    let i = i * 4;
-                    debug_screen[i..i + 3].copy_from_slice(&COLOR[c as usize]);
-                }
-
-                const OLD_COLOR: [[u8; 3]; 4] = [[0, 0, 200], [0, 0, 110], [0, 0, 85], [0, 0, 60]];
-                for i in curr_i..screen.len() {
-                    let c = screen[i];
-                    let i = i * 4;
-                    debug_screen[i..i + 3].copy_from_slice(&OLD_COLOR[c as usize]);
-                }
-
-                proxy
-                    .send_event(UserEvent::UpdateTexture(
-                        textures.screen,
-                        debug_screen.into_boxed_slice(),
-                    ))
-                    .unwrap();
-            }
-
-            let mut tiles = vec![255; 128 * 194 * 4];
-            gameroy::ppu::draw_tiles(
-                &ppu,
-                &mut |x, y, c| {
-                    let i = (x + y * 128) as usize * 4;
-                    let color = COLOR[c as usize];
-                    tiles[i..i + 3].copy_from_slice(&color);
-                },
-                ppu.bgp,
-            );
-            proxy
-                .send_event(UserEvent::UpdateTexture(
-                    textures.tilemap,
-                    tiles.into_boxed_slice(),
-                ))
-                .unwrap();
-
-            let mut background = vec![255; 256 * 256 * 4];
-            gameroy::ppu::draw_background(&ppu, &mut |x, y, c| {
-                let i = (x + y * 256) as usize * 4;
-                background[i..i + 3].copy_from_slice(&COLOR[c as usize]);
-            });
-            proxy
-                .send_event(UserEvent::UpdateTexture(
-                    textures.background,
-                    background.into_boxed_slice(),
-                ))
-                .unwrap();
-
-            let mut window = vec![255; 256 * 256 * 4];
-            gameroy::ppu::draw_window(&ppu, &mut |x, y, c| {
-                let i = (x + y * 256) as usize * 4;
-                window[i..i + 3].copy_from_slice(&COLOR[c as usize]);
-            });
-            proxy
-                .send_event(UserEvent::UpdateTexture(
-                    textures.window,
-                    window.into_boxed_slice(),
-                ))
-                .unwrap();
-
-            for (i, &[view, text]) in self.oam_tiles.iter().enumerate() {
-                let i = i * 4;
-                let data = &ppu.oam[i..i + 4];
-                let sy = data[0] as i32 - 16;
-                let sx = data[1] as i32 - 8;
-                let tile = data[2];
-                let flags = data[3];
-
-                // let palette = if flags & 0x10 != 0 {
-                //     ppu.obp1
-                // } else {
-                //     ppu.obp0
-                // };
-
-                // do a thing
-
-                let x = tile % 16;
-                let y = tile / 16;
-                let uv_rect = [x as f32 / 16.0, y as f32 / 24.0, 1.0 / 16.0, 1.0 / 16.0];
-
-                if let Graphic::Texture(t) = ctx.get_graphic_mut(view) {
-                    if sy < 0 || sx < 0 {
-                        t.color = 0xff0000ff.into();
-                    } else {
-                        t.color = Color::WHITE;
-                    }
-                    t.uv_rect = uv_rect;
-                }
-
-                ctx.get_graphic_mut(text).set_text(&format!(
-                    "x: {:02x} y: {:02x}\ntile: {:02x}\nflag: {:02x}",
-                    sx, sy, tile, flags
-                ))
-            }
+            let emulator_updated = event.is::<EmulatorUpdated>();
+            self.update(ctx, emulator_updated);
         }
     }
 }
@@ -189,7 +192,7 @@ pub fn build(
     let content = ctx
         .create_control()
         .parent(view)
-        .layout(VBoxLayout::new(2.0, [2.0; 4], -1))
+        .layout(VBoxLayout::new(10.0, [2.0; 4], -1))
         .build(ctx);
 
     let v_handle = ctx.reserve();
@@ -221,7 +224,7 @@ pub fn build(
 
     build_tilemap_viewer(ctx, textures.tilemap, style, content, 16, 24, |x, y, _| {
         format!(
-            "tile number: {:02x}\nx: {:02x} y: {:02x}",
+            "tilemap:\ntile number: {:02x}\nx: {:02x} y: {:02x}",
             y as u16 * 16 + x as u16,
             x,
             y
@@ -250,7 +253,10 @@ pub fn build(
                     tile -= 0x100;
                 }
             }
-            format!("tile number: {:02x}\nx: {:02x} y: {:02x}", tile, x, y)
+            format!(
+                "background:\ntile number: {:02x}\nx: {:02x} y: {:02x}",
+                tile, x, y
+            )
         },
     );
 
@@ -269,13 +275,27 @@ pub fn build(
                 tile -= 0x100;
             }
         }
-        format!("tile number: {:02x}\nx: {:02x} y: {:02x}", tile, x, y)
+        format!(
+            "window:\ntile number: {:02x}\nx: {:02x} y: {:02x}",
+            tile, x, y
+        )
     });
 
-    let oam_viewer = ctx
-        .create_control()
+    let oam_viewer = ctx.reserve();
+    ctx.create_control()
         .parent(content)
-        .layout(GridLayout::new([2.0; 2], [2.0; 4], 8))
+        .layout(VBoxLayout::default())
+        .child(ctx, |cb, _| {
+            cb.graphic(Text::new(
+                "Sprites in OAM".to_string(),
+                (-1, 0),
+                style.text_style.clone(),
+            ))
+            .layout(FitText)
+        })
+        .child_reserved(oam_viewer, ctx, |cb, _| {
+            cb.layout(GridLayout::new([2.0; 2], [2.0; 4], 8))
+        })
         .build(ctx);
 
     let oam_tiles: [[Id; 2]; 40] = [(); 40].map(|_| [ctx.reserve(), ctx.reserve()]);
@@ -287,14 +307,12 @@ pub fn build(
             .layout(VBoxLayout::default())
             .graphic(style.background.clone())
             .build(ctx);
-        let x = 0xa;
-        let y = 0xa;
-        let uv_rect = [x as f32 / 16.0, y as f32 / 24.0, 1.0 / 16.0, 1.0 / 16.0];
         ctx.create_control_reserved(view)
             .parent(tile)
-            .graphic(Texture::new(textures.tilemap, uv_rect))
+            .graphic(Texture::new(textures.tilemap, [0.0; 4]))
             .min_size([6.0 * 8.0, 6.0 * 8.0])
             .fill_x(crui::RectFill::ShrinkCenter)
+            .fill_y(crui::RectFill::ShrinkCenter)
             .build(ctx);
         ctx.create_control_reserved(text)
             .parent(tile)
@@ -327,6 +345,12 @@ fn build_tilemap_viewer(
     info: impl Fn(u8, u8, &mut Context) -> String + 'static,
 ) {
     let tilemap_viewer = ctx.reserve();
+    let tilemap_text = ctx
+        .create_control()
+        .parent(tilemap_viewer)
+        .graphic(Text::new(String::new(), (-1, -1), style.text_style.clone()))
+        .min_size([140.0, 16.0])
+        .build(ctx);
     let tilemap = ctx
         .create_control()
         .parent(tilemap_viewer)
@@ -335,12 +359,6 @@ fn build_tilemap_viewer(
         .expand_x(true)
         .fill_x(crui::RectFill::ShrinkStart)
         .fill_y(crui::RectFill::ShrinkCenter)
-        .build(ctx);
-    let tilemap_text = ctx
-        .create_control()
-        .parent(tilemap_viewer)
-        .graphic(Text::new(String::new(), (0, 0), style.text_style.clone()))
-        .min_size([140.0, 16.0])
         .build(ctx);
     ctx.create_control_reserved(tilemap_viewer)
         .parent(parent)

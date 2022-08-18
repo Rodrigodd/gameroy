cee39827b0486329c1dfd1e2c87f6742dd603ae3
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2022-08-17 22:40:11 -0300

    fix 'rerun-if-changed' path in native/build.rs

--- native/build.rs
+++ native/build.rs
@@ -1,9 +1,9 @@
 extern crate embed_resource;
 
 fn main() {
+    println!("cargo:rerun-if-changed=../assets/resources.rc");
     let target = std::env::var("TARGET").unwrap();
     if target.contains("windows") {
-        println!("cargo:rerun-if-changed=assets/resources.rc");
         embed_resource::compile("../assets/resources.rc")
     }
 }

bb32eb82cfe1a3a5512c1872b99bf94c7747e5b5
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2023-04-07 21:56:52 -0300

    fuzz: make fuzz target more lenient to its input
    by filling the missing length if the data has not the exactly size for a
    ROM.

--- fuzz/fuzz_targets/fuzz_cartridge.rs
+++ fuzz/fuzz_targets/fuzz_cartridge.rs
@@ -10,11 +10,45 @@ use gameroy::{
 
 fuzz_target!(|data: &[u8]| {
     // Don't accept data bigger than the biggest possible ROM.
-    if data.len() > 16 * 0x2000 {
+    if data.len() > 256 * 0x2000 {
         return;
     }
 
-    let rom = data.to_vec();
+    // Too small.
+    if data.len() < 0x150 {
+        return;
+    }
+
+    let rom_sizes = [
+        2 * 0x4000, // no ROM Banking
+        4 * 0x4000,
+        8 * 0x4000,
+        16 * 0x4000,
+        32 * 0x4000,
+        64 * 0x4000,
+        128 * 0x4000,
+        256 * 0x4000,
+        // 512 * 0x4000,
+        // 72 * 0x2000,
+        // 80 * 0x2000,
+        // 96 * 0x2000,
+    ];
+
+    // find the next greater possible rom size
+    let rom_size = rom_sizes
+        .into_iter()
+        .enumerate()
+        .rev()
+        .find(|(_, x)| data.len() <= *x)
+        .map(|(i, _)| i)
+        .unwrap();
+
+    let mut rom = data.to_vec();
+    // hardcode it in the data
+    rom[0x0148] = rom_size as u8;
+
+    // fill the remaning size with zeros
+    rom.resize(rom_sizes[rom_size], 0);
 
     let cartridge = match Cartridge::new(rom) {
         Ok(x) => x,
@@ -25,8 +59,8 @@ fuzz_target!(|data: &[u8]| {
     gb.predict_interrupt = true;
     gb.serial.borrow_mut().serial_transfer_callback = None;
 
-    // emulate 2 seconds
-    let target = gb.clock_count + 2 * CLOCK_SPEED;
+    // emulate 100 ms
+    let target = gb.clock_count + CLOCK_SPEED / 10;
 
     while gb.clock_count < target {
         // this should never panic

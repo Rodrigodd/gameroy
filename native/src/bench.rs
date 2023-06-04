86ae123bcb70c8f77ea86f26fe489fd056941565
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2023-06-04 12:50:13 -0300

    bench: include compilation in tests

--- native/src/bench.rs
+++ native/src/bench.rs
@@ -131,13 +131,12 @@ fn run_interpreted(len: usize, game_boy: &mut GameBoy, timeout: u64) -> Vec<Dura
 
 #[cfg(target_arch = "x86_64")]
 fn run_jitted(len: usize, game_boy: &mut GameBoy, timeout: u64) -> Vec<Duration> {
-    let mut jit_compiler = gameroy_jit::JitCompiler::new();
-
-    pre_run(game_boy, timeout, &mut jit_compiler);
+    pre_run(game_boy, timeout, &mut gameroy_jit::JitCompiler::new());
 
     let mut times = Vec::with_capacity(len);
     for _ in 0..len {
         game_boy.reset();
+        let mut jit_compiler = gameroy_jit::JitCompiler::new();
         let start = Instant::now();
         while game_boy.clock_count < timeout {
             jit_compiler.interpret_block(game_boy);

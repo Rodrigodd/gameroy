a8a36be64312a64e9099d69783ac376e11a98f43
samuel-cavalcanti <scavalcanti111@gmail.com>
2023-09-04 09:49:34 -0300

    added resvg to build-dependencies

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use resvg::usvg::TreeParsing;
 
 fn main() {
     render_svg_assets();
@@ -14,23 +14,32 @@ fn render_svg_assets() {
 
     let dir = std::env::var("CARGO_MANIFEST_DIR").expect("set by cargo");
 
-    let out = Command::new("resvg")
-        .args([icons_svg, "assets/icons.png"])
-        .current_dir(&dir)
-        .output()
-        .expect("'resvg' should be installed");
+    let icons = include_bytes!("assets/icons.svg");
 
-    if !out.status.success() {
-        panic!("resvg didnt successed: {:#?}", out);
-    }
+    let opts = resvg::usvg::Options {
+        resources_dir: std::fs::canonicalize(dir).ok(),
+        ..Default::default()
+    };
 
-    let out = Command::new("resvg")
-        .args([icons_svg, "assets/icons2x.png", "--zoom=2"])
-        .current_dir(&dir)
-        .output()
-        .expect("'resvg' should be installed");
+    let tree = resvg::usvg::Tree::from_data(icons, &opts).unwrap();
 
-    if !out.status.success() {
-        panic!("resvg didnt successed: {:#?}", out);
-    }
+    let tree = resvg::Tree::from_usvg(&tree);
+
+    let size = tree.size.to_int_size();
+    let zoom_size = size.scale_by(2.0).unwrap();
+    to_png(&tree, size, "assets/icons.png");
+    to_png(&tree, zoom_size, "assets/icons2x.png");
+}
+
+fn to_png(tree: &resvg::Tree, size: resvg::tiny_skia::IntSize, png_file_name: &str) {
+    let mut pixmap = resvg::tiny_skia::Pixmap::new(size.width(), size.height()).unwrap();
+
+    let size = size.to_size();
+    let transform = resvg::tiny_skia::Transform::from_scale(
+        size.width() / tree.size.width(),
+        size.height() / tree.size.height(),
+    );
+    tree.render(transform, &mut pixmap.as_mut());
+
+    pixmap.save_png(png_file_name).unwrap();
 }

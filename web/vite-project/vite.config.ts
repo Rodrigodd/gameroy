9b634a964dfb2aba2b4a71f2ff0211f390ff1ef7
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2025-03-01 17:33:58 -0300

    Enable HTTPS

--- web/vite-project/vite.config.ts
+++ web/vite-project/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
+  server: {
+    allowedHosts: ["localho.st"],
+    cors: {
+      origin: ["localho.st"],
+      methods: ["GET", "POST"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    }
+  }
 });
